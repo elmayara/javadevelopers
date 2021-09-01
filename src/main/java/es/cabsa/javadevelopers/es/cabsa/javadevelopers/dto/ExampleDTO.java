c346b35e32b07c58afdf222b927807744df8f15e
elmayara <38360433+elmayara@users.noreply.github.com>
2021-09-01 14:25:50 +0200

    Prueba Tecnica

--- src/main/java/es/cabsa/javadevelopers/es/cabsa/javadevelopers/dto/ExampleDTO.java
+++ src/main/java/es/cabsa/javadevelopers/es/cabsa/javadevelopers/dto/ExampleDTO.java
@@ -2,6 +2,46 @@ package es.cabsa.javadevelopers.es.cabsa.javadevelopers.dto;
 
 import java.io.Serializable;
 
+import es.cabsa.javadevelopers.model.Food;
+
 public class ExampleDTO implements Serializable {
 
+	
+	private Long id;
+	private String name;
+	private String legs;
+	private Food food;
+	public ExampleDTO(Long id, String name, String legs, Food food) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.legs = legs;
+		this.food = food;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLegs() {
+		return legs;
+	}
+	public void setLegs(String legs) {
+		this.legs = legs;
+	}
+	public Food getFood() {
+		return food;
+	}
+	public void setFood(Food food) {
+		this.food = food;
+	}
+	
+	
 }
